c19b93196c970a5bbefc9c1917d84ca810b39af8
Maria Theresa <mariatheresahenriques@gmail.com>
2020-10-08 09:16:44 -0300

    Instrucoes tipo Registrador - ainda precisa de modificacoes

--- registers.py
+++ registers.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def RegConverter(Instruction):
+# REGISTRADOR - configuracao em bits
+# funct7    rs2     rs1     funct3      rd      opcode
+# 0000000                   abaixo              0110011
+# 0100000
+
+
+# FUNCAO QUE CONVERTE O CODIGO DE OPERACAO EM BINARIO
+def Funct3(Instruction):
     # R instructions 0110011
     if(Instruction == "add"):
         return "000"
@@ -35,28 +42,21 @@ def RegConverter(Instruction):
     else:
         print("Instrução não encontrada")
 
-    # fun7    rs2 rs1 fun3  rd  opcode
-    # 0000000         acima     0110011
-    # 0100000
-
-    # 0100000 sub sra subw sraw
-    # add 00000000000000000000000000110011
-
-
+# FUNCAO QUE CONVERTE O CODIGO DA FUNCAO DE SETE BITS EM BINARIO
 def Funct7(Instruction):
     if(Instruction == "sub") or (Instruction == "subw") or (Instruction == "sra") or (Instruction == "sraw"):
         return "0000000"
     else:
         return "0100000"
 
-
+# FUNCAO DE CONVERTE O TIPO REGISTRO EM CODIGO BINARIO
 def Register(Instruction):
     funct7 = Funct7(Instruction)
     # funct7 = "0000"
     rs2 = "00000"
     rs1 = "00000"
     rd = "00000"
-    funct3 = RegConverter(Instruction)
+    funct3 = Funct3(Instruction)
     # funct3 = "0000"
     opCode = "0110011"
     codeInstruction = funct7+rs2+rs1+funct3+rd+opCode+"\n"
