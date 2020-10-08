fe26e08a49d841ebabf2a1c01ef63e166f218ff8
Maria Theresa <mariatheresahenriques@gmail.com>
2020-10-08 09:15:41 -0300

    Leitura e escrita de arquivo OK

--- home.py
+++ home.py
@@ -1,7 +1,27 @@
 from registers import*
 
+print("Apenas um teste")
 Instruction = "sub"
-
-print("im here!")
 teste = Register(Instruction)
 print(teste)
+
+
+
+# leitura do arquivo de entrada
+arquivo = open("entrada.asm", "r")
+conteudo = arquivo.read()
+arquivo.close()
+print("Arquivo de entrada:")# print para teste
+print(conteudo)
+
+# escrita do conteudo traduzido no arquivo de saida
+arquivo = open("saida.asm", "w")
+arquivo.write(teste) # retorno esta como teste porque a funcao de retorno ainda nao foi concluida
+arquivo.close()
+
+# mostrar conteudo traduzido no terminal - leitura do arquivo de saida e impressao
+arquivo = open("saida.asm", "r")
+conteudo = arquivo.read()
+arquivo.close()
+print("Arquivo de saida:")  # print para teste
+print(conteudo)
