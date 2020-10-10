674e878e7300b3ba4908f54c453f79ce9ee6f03c
Maria Theresa <mariatheresahenriques@gmail.com>
2020-10-09 22:27:12 -0300

    Configuracao de dicionario ao inves de ifs, configuracao da leitura de arquivo para o formato com ", "

--- home.py
+++ home.py
@@ -1,27 +1,30 @@
+# -*- coding: utf-8 -*-
 from registers import*
 
-print("Apenas um teste")
-Instruction = "sub"
-teste = Register(Instruction)
-print(teste)
-
-
-
 # leitura do arquivo de entrada
-arquivo = open("entrada.asm", "r")
-conteudo = arquivo.read()
+arquivo = open("entrada.asm", 'r')
+# abre arquivo de saida para escrita
+saida = open('saida.asm', 'w')
+# leitura de todas as linhas do arquivo
+linhas = arquivo.readlines()
+for i in linhas:
+    entradaFormatada = i.strip("\n")
+    instrucao = entradaFormatada.split()[0]
+    print(entradaFormatada) # APENAS PARA TESTES!!!
+    rd = entradaFormatada.split(" ")[1]
+    rd = rd.replace(',','') 
+    rs1 = entradaFormatada.split(",")[1]
+    rs1 = rs1.replace(' ', '')
+    rs2 = entradaFormatada.split(", ")[2]
+    binario = TipoR(instrucao, rd, rs1, rs2) # TEM DE TROCAR PELA FUNCAO TRADUTOR QUANDO A FUNCAO IMEDIATA FICAR PRONTA!!!
+    # escrita do conteudo traduzido no arquivo de saida
+    saida.writelines(binario+"\n")
 arquivo.close()
-print("Arquivo de entrada:")# print para teste
-print(conteudo)
-
-# escrita do conteudo traduzido no arquivo de saida
-arquivo = open("saida.asm", "w")
-arquivo.write(teste) # retorno esta como teste porque a funcao de retorno ainda nao foi concluida
-arquivo.close()
+saida.close()
 
 # mostrar conteudo traduzido no terminal - leitura do arquivo de saida e impressao
 arquivo = open("saida.asm", "r")
-conteudo = arquivo.read()
+saida = arquivo.read()
 arquivo.close()
 print("Arquivo de saida:")  # print para teste
-print(conteudo)
+print(saida)
--- registers.py
+++ registers.py
@@ -6,58 +6,95 @@
 # 0100000
 
 
-# FUNCAO QUE CONVERTE O CODIGO DE OPERACAO EM BINARIO
-def Funct3(Instruction):
-    # R instructions 0110011
-    if(Instruction == "add"):
-        return "000"
-    if(Instruction == "sub"):
-        return "000"
-    if(Instruction == "sll"):
-        return "001"
-    if(Instruction == "slt"):
-        return "010"
-    if(Instruction == "sltu"):
-        return "011"
-    if(Instruction == "xor"):
-        return "100"
-    if(Instruction == "srl"):
-        return "101"
-    if(Instruction == "sra"):
-        return "101"
-    if(Instruction == "or"):
-        return "110"
-    if(Instruction == "and"):
-        return "111"
-    if(Instruction == "addw"):
-        return "000"
-    if(Instruction == "subw"):
-        return "000"
-    if(Instruction == "sllw"):
-        return "001"
-    if(Instruction == "srlw"):
-        return "101"
-    if(Instruction == "sraw"):
-        return "101"
-    else:
-        print("Instrução não encontrada")
-
 # FUNCAO QUE CONVERTE O CODIGO DA FUNCAO DE SETE BITS EM BINARIO
-def Funct7(Instruction):
-    if(Instruction == "sub") or (Instruction == "subw") or (Instruction == "sra") or (Instruction == "sraw"):
-        return "0000000"
-    else:
+def Funct7(instrucao):
+    if(instrucao == "sub") or (instrucao == "subw") or (instrucao == "sra") or (instrucao == "sraw"):
         return "0100000"
+    else:
+        return "0000000"
+
+
+# FUNCAO QUE CONVERTE O CODIGO DE OPERACAO EM BINARIO
+def Funct3(instrucao):
+    operacoes = {
+        "add": "000",
+        "sub": "000",
+        "sll": "001",
+        "slt": "010",
+        "sltu": "011",
+        "xor": "100",
+        "srl": "101",
+        "sra": "101",
+        "or": "110",
+        "and": "111",
+        "addw": "000",
+        "subw": "000",
+        "sllw": "001",
+        "srlw": "101",
+        "sraw": "101",
+    }
+    if operacoes[instrucao] == None:
+        print("Operação não encontrada.")
+        return "-1"
+    return operacoes[instrucao]
+
 
 # FUNCAO DE CONVERTE O TIPO REGISTRO EM CODIGO BINARIO
-def Register(Instruction):
-    funct7 = Funct7(Instruction)
-    # funct7 = "0000"
-    rs2 = "00000"
-    rs1 = "00000"
-    rd = "00000"
-    funct3 = Funct3(Instruction)
-    # funct3 = "0000"
-    opCode = "0110011"
-    codeInstruction = funct7+rs2+rs1+funct3+rd+opCode+"\n"
-    return codeInstruction
+def Registrador(registrador):
+    reg = {
+        'x0': '00000',
+        'x1': '00001',
+        'x2': '00010',
+        'x3': '00011',
+        'x4': '00100',
+        'x5': '00101',
+        'x6': '00110',
+        'x7': '00111',
+        'x8': '01000',
+        'x9': '01001',
+        'x10': '01010',
+        'x11': '01011',
+        'x12': '01100',
+        'x13': '01101',
+        'x14': '01110',
+        'x15': '01111',
+        'x16': '10000',
+        'x17': '10001',
+        'x18': '10010',
+        'x19': '10011',
+        'x20': '10100',
+        'x21': '10101',
+        'x22': '10110',
+        'x23': '10111',
+        'x24': '11000',
+        'x25': '11001',
+        'x26': '11010',
+        'x27': '11011',
+        'x28': '11100',
+        'x29': '11101',
+        'x30': '11110',
+        'x31': '11111'
+    }
+
+    if reg[registrador] == None:
+        print("Registrador não encontrado.")
+        return "-1"
+    return reg[registrador]
+
+
+# FUNCAO RESPONSAVEL POR MONTAR AS OPERACOES NOS REGISTRADORES EM BINARIO
+def TipoR(instrucao, rd, rs1, rs2):
+    funct7 = Funct7(instrucao)
+    funct3 = Funct3(instrucao)
+    rd = Registrador(rd)
+    rs1 = Registrador(rs1)
+    rs2 = Registrador(rs2)
+    Optcode = '0110011'
+    return funct7+rs2+rs1+funct3+rd+Optcode
+
+
+# def Tradutor(instrucao, rd, rs1, rs2):
+#     if (instrucao == TipoR):
+#         TipoR(instrucao, rd, rs1, rs2)
+#     # else:
+#         # TipoI(instrucao, rd, rs1, rs2)
